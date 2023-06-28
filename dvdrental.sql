ba7f4241114d0fe3b78dcbbaa75bd2d24a2afd70
Mustafa Eroğlu <85462810+benvekedim@users.noreply.github.com>
2023-06-28 19:50:02 +0300

    Create dvdrental.sql

---
+++ dvdrental.sql
@@ -0,0 +1,27 @@
+SELECT * FROM film;
+
+SELECT * FROM actor;
+
+SELECT film_id, title FROM film;
+
+SELECT * FROM film
+WHERE replacement_cost = 14.99;
+
+SELECT * FROM film 
+WHERE rental_rate = 0.99;
+
+SELECT * FROM actor
+WHERE first_name = 'Penelope';
+
+SELECT * FROM film
+WHERE length > 90;
+
+SELECT * FROM film
+WHERE rental_rate <= 1;
+
+SELECT * FROM film
+WHERE rental_rate !=1;
+
+SELECT * FROM film
+WHERE rental_rate <> 4.99
+
